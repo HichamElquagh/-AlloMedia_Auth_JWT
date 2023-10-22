f240d310228ec7e984e73dd4dd60232298683426
Hichamel <alquaghhicham1@gmail.com>
2023-10-22 19:49:09 +0100

    j'ai ajouter le package jest et j'ai fais l'init aussi j'ai commencer par le testing de register

--- __test__/auth.test.js
+++ __test__/auth.test.js
@@ -3,30 +3,29 @@ const {
     login,
 } = require('../controllers/auth.controller');
 
-// Mock external dependencies and modules
-jest.mock('../models/users'); // Assuming your user model is in user.model.js
-jest.mock('../models/role.model'); // Assuming your role model is in role.model.js
+
+jest.mock('../models/users'); 
+jest.mock('../models/role.model'); 
 jest.mock('bcrypt');
-// jest.mock('../utils/auth.utils'); // Assuming your auth utility functions are in auth.utils.js
+
 
 describe('Auth Controller Tests', () => {
     beforeEach(() => {
-        jest.clearAllMocks(); // Clear mocks before each test
+        jest.clearAllMocks();
     });
 
-    // Test cases for the register function
+    
     describe('register function', () => {
-        // Write your test cases for the register function here
-
+        
 
 
 
 
     });
 
-    // Test cases for the login function
+    
     describe('login function', () => {
-        // Write your test cases for the login function here
+
 
 
 
